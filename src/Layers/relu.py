6dfb28bbdcd5a91f5ad893ac5c874cb289ae368c
Naevyys <siriremerk@gmail.com>
2022-04-22 11:01:55 +0200

    Backward pass of activation functions

--- src/Layers/relu.py
+++ src/Layers/relu.py
@@ -8,16 +8,21 @@ class ReLU(Module):
     def __init__(self):
         super().__init__()
 
-    def __relu(self, x):
+    def forward(self, *inputs):  # Compute activation
         """
-        Applies relu activation function to tensor x
+        Applies relu activation function to tensor inputs
         :param x: Tensor, input of the layer.
         :return: Tensor after applying relu to each value of the tensor.
         """
-        return torch.where(x < 0, torch.tensor(0, dtype=x.dtype), x)
+        x = inputs[0]
+        return torch.where(x <= 0, torch.tensor(0, dtype=x.dtype), x)  # TODO: Change function used to match restrictions of the project
 
-    def forward(self, *inputs):
-        return self.__relu(*inputs)
-
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError  # TODO
+    def backward(self, *gradwrtoutput):  # Compute derivative of activation
+        """
+        Computes derivative of the relu activation function. Note: The derivative of ReLU is undefined at x = 0, so we
+        use the subgradient x' = 0 there.
+        :param gradwrtoutput: Tensor w.r.t. which we compute the derivative.
+        :return: Derivative of the activation.
+        """
+        x = gradwrtoutput[0]
+        return torch.where(x <= 0, torch.tensor(0, dtype=x.dtype), 1)  # TODO: Change function used to match restrictions of the project
--- src/Layers/sigmoid.py
+++ src/Layers/sigmoid.py
@@ -8,16 +8,20 @@ class Sigmoid(Module):
     def __init__(self):
         super().__init__()
 
-    def __sigmoid(self, x):
+    def forward(self, *inputs):  # Compute result of activation
         """
         Applies sigmoid activation function (logistic function) to tensor x
         :param x: Tensor, input of the layer.
         :return: Tensor after applying sigmoid to each value of the tensor.
         """
-        return 1 / (1 + torch.exp(-x))
+        x = inputs[0]
+        return 1 / (1 + torch.exp(-x))  # TODO: Change function used to match restrictions of the project
 
-    def forward(self, *inputs):
-        return self.__sigmoid(*inputs)
-
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError  # TODO
+    def backward(self, *gradwrtoutput):  # Compute derivative of activation
+        """
+        Computes derivative of the sigmoid activation function.
+        :param gradwrtoutput: Tensor w.r.t. which we compute the derivative.
+        :return: Derivative of the activation.
+        """
+        x = gradwrtoutput[0]
+        return torch.exp(x) / (1 + torch.exp(x)) ** 2  # TODO: Change function used to match restrictions of the project
